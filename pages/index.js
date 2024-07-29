6199e997a11f27b615e8879a62c291a7d9084112
Johannes <office@atelierbuchner.at>
2024-07-29 16:51:38 +0200

    news server 2

--- pages/index.js
+++ pages/index.js
@@ -22,8 +22,8 @@ export default function Home({ dataStart, dataNews }) {
     const imageProps = useNextSanityImage(client, dataStart.mainImage);
 
     useEffect(() => {
-        console.log(dataNews);
-    }, []);
+        console.log(dataNews, dataStart);
+    }, [dataNews, dataStart]);
 
     return (
         <MainContainer width="w-full col-span-12 md:col-span-9 md:pl-12 md:ml-[314px] sm:pt-[69px] overflow-hidden">
